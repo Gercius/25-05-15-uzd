8569d6cc85251fd3ef629bd295d44f18ae63cd22
gercius <gerciokaz@gmail.com>
2025-05-16 17:56:14 +0300

    fix cors error

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import errorHandler from "./middleware/errorHandlerMIddleware.js";
 import authRouter from "./routes/authRoutes.js";
 import foodProviderRouter from "./routes/foodProviderRoutes.js";
@@ -9,6 +10,7 @@ import orderRouter from "./routes/orderRoutes.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Alive");
